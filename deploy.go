557bd3f011241e38726740124f578b512449ddef
yuggof <yuggof0@gmail.com>
2016-09-03 13:06:02 +0000

    ...

--- deploy.go
+++ deploy.go
@@ -27,8 +27,7 @@ func getGithubOauthToken() string {
 }
 
 func getVersion() string {
-  fmt.Printf("CIRCLE_BUILD_NUM = %s\n", os.Getenv("CIRCLE_BUILD_NUM"))
-  return os.Getenv("CIRCLE_BUILD_NUM")
+  return fmt.Sprintf("v%d", os.Getenv("CIRCLE_BUILD_NUM"))
 }
 
 func createRelease(githubOauthToken, version string) *release {
